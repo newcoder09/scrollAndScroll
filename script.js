04b394a9ceef3bf7400920ff1dc4d90c7582b2c2
elena souto <elenasouto@gmail.com>
2022-04-06 18:03:42 +0100

    images moving by api dynamically

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-// Api Unsplash
 
-const count = 5;
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+//  Api Unsplash
+const count = 10;
 const apiKey = `eYkjfOuk-RvnO3dC3B5kPtZs87KFR_bM6lvncAqznPk`;
-let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-// Get the photos from unsplash animationPlayState: 
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function displayPhotos() {
+   photosArray.forEach((photo)=> {
+    const item = document.createElement('a');
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', '_blank');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description)
+    img.setAttribute('title', photo.alt_description);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+   });
+}
 
 async function getPhotos() {
-    try {
-       const respnse = await fetch(apiUrl); 
-       const data = await respnse.json();
-       console.log(data);
-    
+try {
+   const response = await fetch(apiUrl);
+   photosArray = await response.json();
+   displayPhotos();
+   // console.log(photosArray)
+  
 } catch (error){
 
 }
+
 }
 
-// on Load
-
-getPhotos()
+getPhotos();
